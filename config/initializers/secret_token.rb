b606e77a67bbbba7ea24265e255155320627a2d9
Takeru Shigemori <sgtakeru@gmail.com>
2012-02-03 00:31:46 +0900

    add rails3.0.11 & .rvmrc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoge::Application.config.secret_token = '61e23e8d82947d8d21207cb3dde0bbe0d935a71cdf4c9d77ebc02e92513577cc767131d80116a4801c60a91396cad539c5c23b33d7f47514de9728ce1c81bc13'
